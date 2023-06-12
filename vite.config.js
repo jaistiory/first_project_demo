ecef0a6fd9431f2fe6f98aa9272a9cea41b39a8b
jaistiory <jaistiory@gmail.com>
2023-06-12 11:58:11 +0800

    fix link

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         'main': resolve(__dirname, 'index.html'),
         'hyperlinks': resolve(__dirname, 'src/demo1/hyperlinks.html'),
         'aboutMe:': resolve(__dirname, 'src/demo1/linkpage/aboutMe.html'),
-        'homework1' : resolve(__dirname, 'src/demo1/linkpage/homework1.html'),
+        'homework_1' : resolve(__dirname, 'src/demo1/linkpage/homework_1.html'),
       }
     }
   }
